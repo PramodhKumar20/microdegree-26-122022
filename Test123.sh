9d8f767b389e5da9635bc1feec49a3bdcc155689
PramodhKumar20 <122678793+PramodhKumar20@users.noreply.github.com>
2023-03-07 22:59:38 +0530

    Test123.sh is created

---
+++ Test123.sh
@@ -0,0 +1,11 @@
+git commends
+git init: Initialize a new Git repository in the current directory.
+git add: Add changes to the staging area for the next commit.
+git commit: Commit changes to the local repository with a message describing the changes.
+git status: Display the current status of the repository, including any uncommitted changes.
+git log: Display a log of all commits made in the repository, including the author, date, and commit message.
+git branch: Create, list, or delete branches.
+git checkout: Switch between different branches or restore files from previous commits.
+git merge: Merge changes from one branch into another.
+git pull: Fetch and merge changes from a remote repository into the current branch.
+git push: Upload local changes to a remote repository.
